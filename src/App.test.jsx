90518ebddaddd7e675850649433da5e951fda131
Matthew Philps <philps.matthew@gmail.com>
2019-10-27 10:04:29 +0900

    enzyme added

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { shallow } from "enzyme";
 import ReactDOM from "react-dom";
 import App from "./App";
 
@@ -6,4 +7,9 @@ it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("works with enzyme", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.text()).toContain("Learn React");
 });
